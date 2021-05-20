ed95b41bfcf121bf36ab55128f0600b1d467133c
pcarreiras <pcarreiras@users.noreply.github.com>
2021-05-20 19:36:58 +0100

    testing

--- src/main/java/com/agora/cicdtests/GreetingController.java
+++ src/main/java/com/agora/cicdtests/GreetingController.java
@@ -3,14 +3,11 @@ package com.agora.cicdtests;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * @author Arun Gupta
- */
 @RestController
 public class GreetingController {
 
     @RequestMapping("/hello")
     public String sayHello() {
-        return "Hello world again";
+        return "Hello world";
     }
 }
